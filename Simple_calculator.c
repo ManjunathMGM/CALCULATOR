adf0667b0480c73501480e8b399ac8deb9d6fa72
Manjunath MGM <manjunathmajety@gmail.com>
2023-01-14 02:24:10 +0530

    Add files via upload

---
+++ Simple_calculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+void operation(double, double, char);
+void main()
+{
+    double a, b;
+    char op;
+
+    printf("Enter the 2 values.\n");
+    scanf("%lf%lf", &a, &b);
+
+    printf("1. Enter '+' to perform addition.\n2. Enter '-' to perform subtraction.\n");
+    printf("3. Enter '*' to perform multiplication\n4. Enter '/' to perform division.\n");
+    printf("5. Enter '%%' to perform modulo.\n6. Enter '^' to calculate power of the number.\n");
+
+    printf("OPERATOR - ");
+    scanf(" %c", &op);
+
+    operation(a, b, op);
+}
+
+void operation(double a, double b, char op)
+{
+    switch (op)
+    {
+    case '+':
+        printf("%.2lf", a + b);
+        break;
+    case '-':
+        printf("%.2lf", a - b);
+        break;
+    case '*':
+        printf("%.2lf", a * b);
+        break;
+    case '/':
+        printf("%.2lf", a / b);
+        break;
+    case '%':
+        printf("%.2lf", fmod(a, b));
+        break;
+    case '^':
+        printf("%.2lf", pow(a, b));
+        break;
+    }
+}
