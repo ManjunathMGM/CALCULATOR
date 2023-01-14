c140e2994e250e6ec9a396fd4c6aff71c1fb2ce0
Manjunath MGM <manjunathmajety@gmail.com>
2023-01-14 22:27:58 +0530

    Update Simple_calculator.c

--- Simple_calculator.c
+++ Simple_calculator.c
@@ -1,49 +1,66 @@
 #include <stdio.h>
 #include <math.h>
-void operation(double, double, char);
 void main() // ~ program to make a simple calculator.
 {
-
-    double a, b;
+    static double a, b;
     char op;
 
-    printf("Enter the 2 values.\n");
-    scanf("%lf%lf", &a, &b);
-
-    printf("1. Enter '+' to perform addition.\n2. Enter '-' to perform subtraction.\n");
-    printf("3. Enter '*' to perform multiplication\n4. Enter '/' to perform division.\n");
-    printf("5. Enter '%%' to perform modulo.\n6. Enter '^' to calculate power of the number.\n");
-
-    printf("OPERATOR - ");
+    scanf("%lf", &a);
     scanf(" %c", &op);
+    scanf("%lf", &b);
 
-    operation(a, b, op);
-}
-
-void operation(double a, double b, char op) // ^ function to perform the operations.
-{
-
-    switch (op)
+    do
     {
-    case '+':
-        printf("%.2lf", a + b);
-        break;
-    case '-':
-        printf("%.2lf", a - b);
-        break;
-    case '*':
-        printf("%.2lf", a * b);
-        break;
-    case '/':
-        printf("%.2lf", a / b);
-        break;
-    case '%':
-        printf("%.2lf", fmod(a, b));
-        break;
-    case '^':
-        printf("%.2lf", pow(a, b));
-        break;
-    }
+        switch (op)
+        {
+
+        case '+': // ? ADDITION
+            printf("%.2lf", a + b);
+            a = a + b;
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+
+        case '-': // ? SUBTRACTION
+            printf("%.2lf", a - b);
+            a = a - b;
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+
+        case '*': // ? MULTIPLICATION
+            printf("%.2lf", a * b);
+            a = a * b;
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+
+        case '/': // ? DIVISION
+            printf("%.2lf", a / b);
+            a = a / b;
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+
+        case '%': // ? MODULO DIVISION
+            printf("%.2lf", fmod(a, b));
+            a = fmod(a, b);
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+
+        case '^': // ? EXPONENT VALUE
+            printf("%.2lf", pow(a, b));
+            a = pow(a, b);
+            scanf(" %c", &op);
+            scanf("%lf", &b);
+            break;
+        }
+        // ^ Press '=' twice to end the program.
+    } while (op != '=');
+
+    if (op == '=')
+        printf("DONE");
 }
 
 // & THIS CODE IS WRITTEN BY MANJUNATH MGM.
